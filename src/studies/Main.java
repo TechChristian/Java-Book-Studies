2e49961a5d7133f3fc8683136c26ceca51d928b4
Christian Lopes <christianx111.ls@gmail.com>
2026-02-26 15:12:42 -0300

    estudos de while

--- src/studies/Main.java
+++ src/studies/Main.java
@@ -1,3 +1,4 @@
+package studies;
 
 public class Main {
     public static void main(String[] args) {
--- src/studies/loops/Loop.java
+++ src/studies/loops/Loop.java
@@ -1,14 +1,14 @@
-package studies;
+package studies.loops;
 
 public class Loop {
     public static void main(String[] args) {
         int x = 1;
         System.out.println("Before the loop");
-
+        // Enquanto x for menor que 4, as instruções serão executadas
         while (x < 4){
             System.out.println("in the loop");
             System.out.println("Value of x is " + x);
-            x = x + 1;
+            x = x + 1; // a cada loop "True" será adicionado 1 ao X
         }
         System.out.println("This is after the loop " + x);
     }
