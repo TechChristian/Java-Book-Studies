dbe933c3d5127cdfe6b106054f83ec6da1c10ff3
Christian Lopes <christianx111.ls@gmail.com>
2026-02-26 14:41:41 -0300

    Estrutura While

---
+++ src/studies/Loop.java
@@ -0,0 +1,15 @@
+package studies;
+
+public class Loop {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Before the loop");
+
+        while (x < 4){
+            System.out.println("in the loop");
+            System.out.println("Value of x is " + x);
+            x = x + 1;
+        }
+        System.out.println("This is after the loop " + x);
+    }
+}
