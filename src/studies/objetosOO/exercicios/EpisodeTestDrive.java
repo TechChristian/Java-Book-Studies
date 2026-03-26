1a11882ef16f675a800b049b8a9b49125ddcaac2
Christian <christian@MacBook-Air-de-Christian.local>
2026-03-26 16:33:11 -0300

    Exercicios -> refatorando o codigo do livro, resolvendo problemas de compilacao e organizando de uma forma melhor

---
+++ src/studies/objetosOO/exercicios/EpisodeTestDrive.java
@@ -0,0 +1,30 @@
+package studies.objetosOO.exercicios;
+
+public class EpisodeTestDrive {
+    public static void main(String[] args){
+        Episode episode = new Episode();
+
+        episode.seriesName = "Dexter";
+        episode.episodeNumber = 84;
+
+        episode.play();
+        episode.skipInto();
+        episode.skipToNext();
+    }
+}
+
+class Episode{
+    String seriesName;
+    int episodeNumber;
+
+    void play(){
+        System.out.println("Starting the series : " + seriesName);
+    }
+    void skipInto(){
+        System.out.println("Skipping into...");
+    }
+    void skipToNext(){
+        episodeNumber = episodeNumber + 1;
+        System.out.println("Loading next episode... " + episodeNumber );
+    }
+}
