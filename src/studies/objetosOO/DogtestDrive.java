ae10590009aff49958f9ac5efcbe0715cae2314a
Christian <christian@MacBook-Air-de-Christian.local>
2026-03-26 15:08:36 -0300

    exercicios de criacao de OO, instanciando, criando metodos e testando na classe Main

--- src/studies/objetosOO/DogtestDrive.java
+++ src/studies/objetosOO/DogtestDrive.java
@@ -14,3 +14,23 @@ public class DogtestDrive {
         dog.bark();
     }
 }
+    class Dog {
+        // -> variaveis de instancia (o que a classe conhece)
+        int size;
+        String bread;
+        String name;
+
+        // -> Metodos (o que a classe faz)
+        void bark(){
+            System.out.println("Ruff! Ruff!");
+        }
+
+        void run(){
+            System.out.println(name + " Running");
+        }
+
+        void sleep(){
+            System.out.println(name + " Sleeping");
+        }
+    }
+
---
+++ src/studies/objetosOO/exercicios/StreamingSongTestDrive.java
@@ -0,0 +1,24 @@
+package studies.objetosOO.exercicios;
+
+class StreamingSong {
+    String title;
+    String artist;
+    int duration;
+
+    void play(){
+        System.out.println("Playing song");
+    }
+    void printDetails(){
+        System.out.println("This is " + title + " by " + artist);
+    }
+}
+
+public class StreamingSongTestDrive{
+    public static void main(String[] args){
+        StreamingSong song = new StreamingSong();
+        song.title = "Raindance";
+        song.artist = "Dave";
+        song.duration = 3;
+        song.printDetails();
+    }
+}
