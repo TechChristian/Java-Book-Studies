fc41da443ffcc22ff12cfe265355b0220cfe0b36
Christian Lopes Ramos <christian@MacBook-Air-de-Christian.local>
2026-03-26 11:47:36 -0300

    criando objetos e instancias da classe movie

---
+++ src/studies/objetosOO/MovieTestDrive.java
@@ -0,0 +1,30 @@
+package studies.objetosOO;
+
+public class MovieTestDrive {
+    public static void main(String[] args){
+        Movie one = new Movie();
+        one.title = "Avengers";
+        one.genre = "Action";
+        one.rating = 200;
+
+        Movie two = new Movie();
+        two.title = "Big people two";
+        two.genre = "Comedy";
+        two.rating = 500;
+        two.playIt();
+        two.pause();
+    }
+}
+
+class Movie{
+    String title;
+    String genre;
+    int rating;
+
+    void playIt(){
+        System.out.println("Playing the movie " + title );
+    }
+    void pause(){
+        System.out.println("Pausing the movie " + title);
+    }
+}
