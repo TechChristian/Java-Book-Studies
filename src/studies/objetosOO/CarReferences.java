0f29a2a31cba64349e70cf29a28a139bd6d4006f
Christian <christianx111.ls@gmail.com>
2026-05-05 23:19:56 -0300

    praticando referencias a objetos e criacao de arrays, a partir da classe CarReferences

---
+++ src/studies/objetosOO/CarReferences.java
@@ -0,0 +1,35 @@
+package studies.objetosOO;
+
+public class CarReferences {
+    String model;
+    String color;
+
+    public static void main(String[] args){
+        CarReferences car1 = new CarReferences();
+        car1.model = "Mustang";
+        car1.color = "Red";
+
+        CarReferences[] car = new CarReferences[3];
+        car[0] = new CarReferences();
+        car[1] = new CarReferences();
+
+        car[0].model = "Ferrari";
+        car[0].color = "Red";
+        car[1].model = "Porsche";
+        car[1].color = "White";
+        car[2] = car1;
+
+        System.out.println("Last car's model is " + car[2].model + "and color is " + car[2].color);
+
+        int x = 0;
+
+        while(x < car.length){
+            car[x].run();
+            x = x + 1;
+        }
+    }
+
+    public void run(){
+        System.out.println(model + " Running...");
+    }
+}
